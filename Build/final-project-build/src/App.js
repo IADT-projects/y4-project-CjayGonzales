0f46d8298515cb56d0683756db32e471d3d88304
Clemente Gonzales <cjaygonzales117@gmail.com>
2023-03-07 11:22:36 +0000

    adding boiler plate pages

--- Build/final-project-build/src/App.js
+++ Build/final-project-build/src/App.js
@@ -1,8 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+
+// importing pages
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import PageNotFound from './pages/PageNotFound';
 
 const App = () => {
   return (
     <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
 
+      </Router>
     </div>
   );
 }
