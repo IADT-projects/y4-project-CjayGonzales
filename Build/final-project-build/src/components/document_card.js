52b7b60208c0d39dcda38549d724c50cfaf608e1
Clemente Gonzales <cjaygonzales117@gmail.com>
2023-03-13 10:55:24 +0000

    card props are returing as undefined

--- Build/final-project-build/src/components/document_card.js
+++ Build/final-project-build/src/components/document_card.js
@@ -3,15 +3,15 @@ import React from "react";
 function DocumentCard(props) {
 
     // COMMENT THE BELOW OUT TO FIX
-    console.log("card props " + props.document._id)
+    console.log("card props " + props.document)
 
-    // let ID = <p><b>ID: </b>{props.document._id}</p>
+    let ID = <p><b>ID: </b>{props.document}</p>
 
 
     return (
         <div className="card">
             <div className="card-body">
-                {/* <h5 className="card-title">{ID}</h5> */}
+                <h5 className="card-title">{ID}</h5>
 
             </div>
         </div>
