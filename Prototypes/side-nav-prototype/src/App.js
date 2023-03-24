f89ae54ee4640b43f99323ce6f2aba1a982da8c3
Clemente Gonzales <cjaygonzales117@gmail.com>
2023-03-24 12:44:44 +0000

    sidebar prototype

---
+++ Prototypes/side-nav-prototype/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import SideNav2 from './SideNav2';
+function App() {
+  return (
+    <>
+      <SideNav2 />
+    </>
+  );
+}
+
+export default App;
---
+++ Prototypes/side-nav-prototype/src/SideNav2.js
@@ -0,0 +1,51 @@
+import React from "react";
+import Sidebar from "react-sidebar";
+
+class NavBar2 extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            sidebarOpen: false
+        };
+        this.onSetSidebarOpen = this.onSetSidebarOpen.bind(this);
+    }
+
+    onSetSidebarOpen(open) {
+        this.setState({ sidebarOpen: open });
+    }
+
+    render() {
+        return (
+            <Sidebar
+                sidebar={
+                    <>
+                        <b>Sidebar content</b>
+                        <br />
+                        <a href="https://www.youtube.com/">Hello</a>
+                        <h1>
+                            Hello
+                        </h1>
+                        <button onClick={() => this.onSetSidebarOpen(false)}> Close sidebar</button>
+                    </>
+
+
+                }
+                open={this.state.sidebarOpen}
+                onSetOpen={this.onSetSidebarOpen}
+                styles={{
+                    sidebar: {
+                        background: "black",
+                        color: "white",
+                        width: "300px"
+                    }
+                }}
+            >
+                <button onClick={() => this.onSetSidebarOpen(true)}>
+                    Open sidebar
+                </button>
+            </Sidebar>
+        );
+    }
+}
+
+export default NavBar2;
