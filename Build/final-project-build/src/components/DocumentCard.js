d8ce40c3f5e1f4fa8a0002ea25109db757da4258
Clemente Gonzales <cjaygonzales117@gmail.com>
2023-03-26 12:47:38 +0100

    images now display when viewing

--- Build/final-project-build/src/components/DocumentCard.js
+++ Build/final-project-build/src/components/DocumentCard.js
@@ -5,9 +5,11 @@ import DeleteBtn from "./modals/documentModals/DeleteDocument";
 
 const DocumentCard = (props) => {
     const documentID = props.document._id
+    const STATIC_FILES_URL = 'https://final-project-bucket-v2.s3.eu-west-1.amazonaws.com/'
     // creating an ID and returning it as html. the user is able to access the file through an ID link
     // let ID = <p><b></b><Link to={`/documents/${props.document._id}`}>{props.document._id}</Link></p>
     let title = <p><b>Title: </b><Link to={`/documents/${documentID}`}>{props.document.title}</Link></p>
+    let imgPath = props.document.imgPath
 
     const [show, setShow] = useState(false)
 
@@ -16,6 +18,7 @@ const DocumentCard = (props) => {
             <div className="card-body">
                 {/* <h5 >{ID}</h5> */}
                 <h5 >{title}</h5>
+                <img src={`${STATIC_FILES_URL}${imgPath}`} width="150" height="200"></img>
 
                 <button onClick={() => setShow(true)}>Edit</button>
                 <EditDocument show={show} documentID={documentID} />
--- Build/final-project-build/src/components/ocrComponents/ViewAllOcrCard.js
+++ Build/final-project-build/src/components/ocrComponents/ViewAllOcrCard.js
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom';
 
 const OcrCard = (props) => {
     const ocrID = props.ocrs._id
+    const STATIC_FILES_URL = 'https://final-project-bucket-v2.s3.eu-west-1.amazonaws.com/'
     // creating an ID and returning it as html. the user is able to access the file through an ID link
-    // let ID = <p><b></b><Link to={`/documents/${props.document._id}`}>{props.document._id}</Link></p>
     let result = <p><b>Result: </b><Link to={`/ocr/${ocrID}`}>{props.ocrs.result}</Link></p>
-
+    let imgPath = props.ocrs.imgPath
     const [show, setShow] = useState(false)
 
     return (
@@ -14,7 +14,7 @@ const OcrCard = (props) => {
             <div className="card-body">
                 {/* <h5 >{ID}</h5> */}
                 <h5 >{result}</h5>
-
+                <img src={`${STATIC_FILES_URL}${imgPath}`} width="500" height="600"></img>
             </div>
         </div>
     );
